eeb83f1a8c0eb6b8f61c0766021ca9e2d4aaba24
Ben <geumyong@carrotins.com>
2024-01-13 22:04:54 +0900

    react native랑 native module 연결 테스트

--- App.tsx
+++ App.tsx
@@ -5,114 +5,167 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useCallback, useState} from 'react';
 import {
+  NativeModules,
+  Pressable,
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
+  useWindowDimensions,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const screenSize = useWindowDimensions();
+  const buttonSize = screenSize.width / 4;
+
+  const [resultNum, setResultNum] = useState('');
+  const [inputNum, setInputNum] = useState('');
+  const [tempNum, setTempNum] = useState(0);
+  const [lastAction, setLastAction] = useState<string | null>(null);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const onPressNumber = useCallback(
+    (pressed: number) => {
+      console.log(pressed);
+
+      if (resultNum !== '') {
+        setResultNum('');
+      }
+
+      setInputNum(prevState => {
+        const nextNum = parseInt(`${prevState}${pressed}`);
+        return nextNum.toString();
+      });
+    },
+    [resultNum],
+  );
+
+  const onPressAction = useCallback(
+    async (action: string) => {
+      console.log(action);
+
+      if (action === 'clear') {
+        setInputNum('');
+        setTempNum(0);
+        setResultNum('');
+        return;
+      }
+
+      if (action === 'equal') {
+        if (tempNum !== 0) {
+          const result = await NativeModules.CalculatorModule.excuteCalc(
+            lastAction,
+            tempNum,
+            parseInt(inputNum),
+          );
+
+          console.log(result);
+          setResultNum(result.toString());
+          setTempNum(0);
+        }
+        return;
+      }
+
+      setLastAction(action);
+
+      if (resultNum !== '') {
+        setTempNum(parseInt(resultNum));
+        setResultNum('');
+        setInputNum('');
+      } else if (tempNum === 0) {
+        setTempNum(parseInt(inputNum));
+        setInputNum('');
+      } else {
+        const result = await NativeModules.CalculatorModule.excuteCalc(
+          action,
+          tempNum,
+          parseInt(inputNum),
+        );
+        console.log(result);
+        setResultNum(result.toString());
+        setTempNum(0);
+      }
+    },
+    [inputNum, lastAction, resultNum, tempNum],
+  );
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={{flex: 1}}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+          <Text style={{fontSize: 48, padding: 48}}>
+            {resultNum !== '' ? resultNum : inputNum}
+          </Text>
         </View>
-      </ScrollView>
+
+        <View style={{flex: 1, flexDirection: 'row'}}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginRight: 4,
+            }}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(number => (
+              <Pressable
+                style={{
+                  width: buttonSize - 4,
+                  height: buttonSize - 4,
+                  borderRadius: (buttonSize - 4) * 0.5,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: 'gray',
+                }}
+                onPress={() => onPressNumber(number)}>
+                <Text style={{fontSize: 24}}>{number}</Text>
+              </Pressable>
+            ))}
+          </View>
+
+          <View style={{paddingHorizontal: 12}}>
+            {[
+              {label: '+', action: 'plus'},
+              {label: '-', action: 'minus'},
+              {label: '*', action: 'multiply'},
+              {label: '/', action: 'divide'},
+              {label: 'C', action: 'clear'},
+              {label: '=', action: 'equal'},
+            ].map(action => {
+              return (
+                <Pressable
+                  style={{
+                    width: screenSize.width / 6,
+                    height: screenSize.width / 6,
+                    borderRadius: (screenSize.width / 6) * 0.5,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: 'gray',
+                  }}
+                  onPress={() => onPressAction(action.action)}>
+                  <Text style={{fontSize: 24}}>{action.label}</Text>
+                </Pressable>
+              );
+            })}
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
